0f1d764d10b6e4fc02486408cea28bb8ff21083d
PRASANNA <PRASANNA@LAPTOP-I0I59B7L>
2025-06-22 21:35:34 +0530

    change the naming convention

--- src/body.jsx
+++ src/body.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Footer from "./footer";
 import axios from "axios";
 import { base_url } from "./utils/constants";
